6c5b33a6dc0e58b14a41a96c769a6329578936e6
Нина Евлаш <evlash.nina@mail.ru>
2025-11-01 19:27:20 +0100

    feat: implement readFile function (move up in fs)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readline from 'readline';
 import { goUP, goToDir } from './nwd/navigation.js';
 import { listDir } from './nwd/list.js';
+import {  readFile } from './fs/read.js';
 
 const args = process.argv.slice(2);
 const usernameArg = args.find(arg => arg.startsWith('--username='));
@@ -20,7 +21,7 @@ console.log(`You are currently in ${process.cwd()}`);
 rl.prompt();
 
 
-rl.on('line', (line) => {
+rl.on('line', async (line) => {
   const consoleText = line.trim();
   try {
     if (consoleText === '.exit') {
@@ -33,6 +34,9 @@ rl.on('line', (line) => {
       goToDir(pathTo);
     } else if (consoleText.startsWith('ls')) {
       listDir();
+    } else if (consoleText.startsWith('cat ')) {
+      const nameFile = consoleText.slice(4).trim();
+      await readFile(nameFile);
     } else {
       console.log(`Invalid input!`);
     }
